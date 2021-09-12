7dde870c9316dcf87c8f2d2c17802fe203f663aa
willardsigei <kipngeno.sigei@student.moringaschool.com>
2021-09-12 16:04:19 +0300

    addded js content

--- index.js
+++ index.js
@@ -13,3 +13,43 @@ $(document).ready(function(){
       $("#product").toggle();
     });
 });
+$("#work1").mouseover(function(){
+    $("#overlay").show();
+  }).mouseout(function(){
+    $("#overlay").hide();
+  });
+  $("#work2").mouseover(function(){
+    $("#overlay2").show();
+  }).mouseout(function(){
+    $("#overlay2").hide();
+  });
+  $("#work3").mouseover(function(){
+    $("#over3").show();
+  }).mouseout(function(){
+    $("#over3").hide();
+  });
+  $("#work4").mouseover(function(){
+    $("#overlay4").show();
+  }).mouseout(function(){
+    $("#overlay4").hide();
+  });
+  $("#work5").mouseover(function(){
+    $("#overlay5").show();
+  }).mouseout(function(){
+    $("#overlay5").hide();
+  });
+  $("#work6").mouseover(function(){
+    $("#overlay6").show();
+  }).mouseout(function(){
+    $("#overlay6").hide();
+  });
+  $("#work7").mouseover(function(){
+    $("#overlay7").show();
+  }).mouseout(function(){
+    $("#overlay7").hide();
+  });
+  $("#work8").mouseover(function(){
+    $("#overlay8").show();
+  }).mouseout(function(){
+    $("#overlay8").hide();
+  });
