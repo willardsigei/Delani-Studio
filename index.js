64137aa38684b63b473d662c1a3870239eced1ba
willardsigei <kipngeno.sigei@student.moringaschool.com>
2021-09-12 16:04:48 +0300

    addded js content

--- index.js
+++ index.js
@@ -52,4 +52,16 @@ $("#work1").mouseover(function(){
     $("#overlay8").show();
   }).mouseout(function(){
     $("#overlay8").hide();
+  });  $(document).ready(function(){
+    $("form#message-form").submit(function(){
+      var name = $("input#input1").val();
+      if ($("input#input1").val() && $("input#input2").val()){
+        alert(name + ",thank you for reaching out to us. We will get back to you soon.");
+      }
+      else {
+        alert("Please enter your name and email address!");
+      }
+      
+    });
+  
   });
